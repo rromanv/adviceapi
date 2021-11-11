ec4ca13bef2207d2e12399c3b45cf4e6ecfc2717
Rodolfo Roman <rromanv@gmail.com>
2021-11-10 22:23:44 -0600

    Layout Done

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,29 @@
 <template>
-  <h1>Home Page</h1>
+  <div>
+    <input
+      type="text"
+      placeholder="Search and Advise..."
+      class="w-full py-4 mt-16 text-xl text-center rounded-full"
+    />
+    <div class="grid grid-cols-2 gap-16 pt-16">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi illum
+        accusamus explicabo, delectus tenetur expedita aut inventore quos fugit
+        nulla cumque adipisci temporibus quaerat iure dolorum ut dolore dolorem
+        autem.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi illum
+        accusamus explicabo, delectus tenetur expedita aut inventore quos fugit
+        nulla cumque adipisci temporibus quaerat iure dolorum ut dolore dolorem
+        autem.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi illum
+        accusamus explicabo, delectus tenetur expedita aut inventore quos fugit
+        nulla cumque adipisci temporibus quaerat iure dolorum ut dolore dolorem
+        autem.
+      </p>
+    </div>
+  </div>
 </template>
--- src/router.js
+++ src/router.js
@@ -1,8 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import Index from "./pages/index.vue";
-import About from "./pages/about.vue";
-import Hello from "./pages/hello.vue";
 import NotFound from "./pages/404.vue";
 
 const routes = [
@@ -10,16 +8,6 @@ const routes = [
     path: "/",
     name: "Index",
     component: Index,
-  },
-  {
-    path: "/about",
-    name: "About",
-    component: About,
-  },
-  {
-    path: "/hello",
-    name: "Hello",
-    component: Hello,
   },
   {
     path: "/:pathMatch(.*)*",
