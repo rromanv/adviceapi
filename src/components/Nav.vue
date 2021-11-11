4e0cfd384119ba49b5acfdcd18f1821bd32fd200
Rodolfo Roman <rromanv@gmail.com>
2021-11-10 22:19:34 -0600

    Custom Changes

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,46 +1,9 @@
 <template>
-  <div class="bg-green-800 text-green-200">
-    <div class="container mx-auto flex items-center justify-between">
-      <h1 class="tracking-tighter text-3xl font-thin">
-        Basic<span class="font-normal">Router</span>
+  <div class="text-green-200 bg-green-800">
+    <div class="container flex items-center justify-center mx-auto">
+      <h1 class="py-8 text-3xl font-thin tracking-tighter">
+        Advice<span class="font-normal">Call</span>
       </h1>
-      <nav>
-        <ul class="flex space-x-4">
-          <router-link to="/">
-            <li
-              class="
-                py-8
-                px-4
-                hover:cursor-pointer hover:bg-green-500 hover:text-green-800
-              "
-            >
-              Home
-            </li>
-          </router-link>
-          <router-link :to="{ name: 'About' }">
-            <li
-              class="
-                py-8
-                px-4
-                hover:cursor-pointer hover:bg-green-500 hover:text-green-800
-              "
-            >
-              About
-            </li>
-          </router-link>
-          <router-link :to="{ path: '/hello', name: 'Hello' }">
-            <li
-              class="
-                py-8
-                px-4
-                hover:cursor-pointer hover:bg-green-500 hover:text-green-800
-              "
-            >
-              Hello
-            </li>
-          </router-link>
-        </ul>
-      </nav>
     </div>
   </div>
 </template>
