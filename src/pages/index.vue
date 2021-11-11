207d2db1c6976def048e2e4ffcd0a037c35a3948
Rodolfo Roman <rromanv@gmail.com>
2021-11-10 23:06:17 -0600

    Adfice Call done

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,29 +1,27 @@
+<script setup>
+import { ref } from "vue";
+import { useAdvice } from "../composables/useAdvice";
+
+const { advices, search } = useAdvice();
+
+const searchItem = ref("");
+</script>
+
 <template>
   <div>
     <input
+      v-model="searchItem"
+      @change="search(searchItem)"
       type="text"
-      placeholder="Search and Advise..."
+      placeholder="Search an Advise..."
       class="w-full py-4 mt-16 text-xl text-center rounded-full"
     />
     <div class="grid grid-cols-2 gap-16 pt-16">
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi illum
-        accusamus explicabo, delectus tenetur expedita aut inventore quos fugit
-        nulla cumque adipisci temporibus quaerat iure dolorum ut dolore dolorem
-        autem.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi illum
-        accusamus explicabo, delectus tenetur expedita aut inventore quos fugit
-        nulla cumque adipisci temporibus quaerat iure dolorum ut dolore dolorem
-        autem.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi illum
-        accusamus explicabo, delectus tenetur expedita aut inventore quos fugit
-        nulla cumque adipisci temporibus quaerat iure dolorum ut dolore dolorem
-        autem.
-      </p>
+      <div v-for="(advice, index) in advices" :key="index">
+        <p>
+          {{ advice.advice }}
+        </p>
+      </div>
     </div>
   </div>
 </template>
